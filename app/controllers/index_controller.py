b3476d501a108afe099762b08e96b3db58a741e4
Nadzeya Kuzmitch <nk83@njit.edu>
2022-02-08 10:54:10 -0500

    Updated my name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Nadzeya K"
+        name = "Nadzeya"
         return render_template('index.html', name=name)
